d28c7d10565531f77404386ffd2b059c0b24c72a
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-10-21 19:50:13 +0600

    added route and setup file in index.js file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * Author: Md Rakib Mia
  */
 
+
 /* external imports */
 const express = require("express");
 const cors = require("cors");
@@ -10,7 +11,59 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 require("dotenv").config();
 
+/* internal imports */
+const errorHandler = require("./middleware/error.middleware");
+const dbConnection = require("./utils/db.util");
 
+/* custom colors setup */
+colors.setTheme({
+  success: "green",
+  error: "red",
+});
+
+/* router level imports */
+const jobRoute = require("./routes/job.route");
+const userRoute = require("./routes/user.route");
+const managerRoute = require("./routes/manager.route");
+
+/* application level connections */
+const app = express();
+const port = process.env.PORT || 8080;
+
+/* middleware connections */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* router level connections */
+app.use("/jobs", jobRoute);
+app.use("/user", userRoute);
+app.use("/manager", managerRoute);
+
+/* global error handler */
+app.use(errorHandler);
+
+/* database connection */
+// dbConnection();
+mongoose
+  .connect(process.env.DB_URI, {
+    dbName: "job-portal-assignment",
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Success: MongoDB connected with Mongoose".green.bold.italic);
+  })
+  .catch((error) => console.log(`Error: ${error.name}`.error.bold));
+
+/* enable backend connection */
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    acknowledgement: true,
+    message: "OK",
+    description: "The request is OK",
+  });
+});
 
 /* enable server */
 app.listen(port, () => {
@@ -18,3 +71,4 @@ app.listen(port, () => {
     colors.green.italic.bold(`Success: Job portal connected on port ${port}`)
   );
 });
+
