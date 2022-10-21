93953eaee076ac3ca8179c30177ba403bbbe4453
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-10-21 19:22:15 +0600

    create index.js file and add external importrs

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Title: Job Portal Assignment
+ * Author: Md Rakib Mia
+ */
+
+/* external imports */
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const colors = require("colors");
+require("dotenv").config();
+
